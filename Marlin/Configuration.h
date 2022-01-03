ee332079948d96cff763dbfd52288253315fcb0f
Alex Levin <alevin@gmail.com>
2022-01-03 09:49:40 -0800

    name change

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -145,7 +145,7 @@
 
 // Name displayed in the LCD "Ready" message and Info menu
 //#define CUSTOM_MACHINE_NAME "3D Printer"
-#define CUSTOM_MACHINE_NAME "P3_SKR1.4-2.0.9-07D"
+#define CUSTOM_MACHINE_NAME "P3_SKR-2.0.9-08"
 
 // Printer's unique ID, used by some programs to differentiate between machines.
 // Choose your own or use a service like https://www.uuidgenerator.net/version4
